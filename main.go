5a085392f215126b5e852b6825625e86ff97eddd
Philip Örnfeldt <philip.ornfeldt@outlook.com>
2022-02-12 19:48:19 +0100

    Testing my way forward

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+  "github.com/rivo/tview"
+)
+
+func GetForm() *tview.Form {
+  app := tview .NewApplication()
+  form := tview.NewForm().
+    AddInputField("Username", "", 20, nil, nil).
+    AddPasswordField("Password", "", 20, '*', nil).
+    AddButton("Submit", func() {
+      app.Stop()
+    })
+  form.SetBorder(true).SetTitle("Submit username/passwod").SetTitleAlign(tview.AlignLeft)
+  if err := app.SetRoot(form, false).SetFocus(form).Run(); err != nil {
+    panic(err)
+  }
+  return form
+}
 
 func main() {
-
+  dataForm := GetForm()
+  print(dataForm.GetFormItemByLabel("Username"))
 }
